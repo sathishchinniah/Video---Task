6465fc3980f274a6ed77d9490fb002ff81962648
sathish chinniah <sathish73ec@gmail.com>
2015-11-20 12:16:48 +0530

    Initial Commit

---
+++ Video Task 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Video Task 2
+//
+//  Created by Sathish Chinniah on 20/11/15.
+//  Copyright © 2015 Sathish Chinniah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Video Task 2Tests/Video_Task_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Video_Task_2Tests.swift
+//  Video Task 2Tests
+//
+//  Created by Sathish Chinniah on 20/11/15.
+//  Copyright © 2015 Sathish Chinniah. All rights reserved.
+//
+
+import XCTest
+@testable import Video_Task_2
+
+class Video_Task_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
